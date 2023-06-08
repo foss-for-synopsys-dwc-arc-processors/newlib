d9abfd08c3dc9adcc8bc4403a73b0bead310d10c
Yuriy Kolerov <ykolerov@synopsys.com>
2023-06-08 13:37:20 +0400

    arc: Switch from DW UART to UART 8250

--- libgloss/arc/read.h
+++
@@ -1,6 +0,0 @@
-#include <sys/types.h>
-
-#define xdev_in(func) xfunc_in = (unsigned char(*)(void))(func)
-extern unsigned char (*xfunc_in)(void);
-
-_ssize_t _read(int fd, void *buffer, unsigned int count);
--- libgloss/arc/write.h
+++
@@ -1,7 +0,0 @@
-#include <sys/types.h>
-
-#define xdev_out(func) xfunc_out = (void(*)(unsigned char))(func)
-extern void (*xfunc_out)(unsigned char);
-extern void _dw_putc (char c);
-
-extern _ssize_t _write(int fd, const void *buffer, unsigned int count);
