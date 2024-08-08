7f578754fb1c79b7f78a4dd26f1ae7a3d1a27da4
Yuriy Kolerov <ykolerov@synopsys.com>
2024-08-09 00:14:03 +0300

    libgloss: riscv: Add support of semihosting command line arguments
    Signed-off-by: Yuriy Kolerov <ykolerov@synopsys.com>

--- libgloss/riscv/semihost-sys_fdtable.c
+++ libgloss/riscv/semihost-sys_fdtable.c
@@ -2,10 +2,14 @@
  * Copyright (C) 2020 Embecosm Limited
  * SPDX-License-Identifier: BSD-2-Clause
  */
-#include "semihost_fdtable.h"
 #include <errno.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <string.h>
+#include <ctype.h>
+#include <machine/syscall.h>
+#include "semihost_syscall.h"
+#include "semihost_fdtable.h"
 
 #ifndef RISCV_MAX_OPEN_FILES
 #define RISCV_MAX_OPEN_FILES 16
@@ -21,8 +25,8 @@ static struct fdentry fdtable[RISCV_MAX_OPEN_FILES];
 
 /* Initialize fdtable.  A handle of -1 denotes an empty entry.  */
 
-void __attribute__ ((constructor))
-init_semihosting ()
+static void
+init_semihosting_fdtable ()
 {
   int i;
 
@@ -45,6 +49,75 @@ init_semihosting ()
   i = _open (":tt", O_WRONLY|O_CREAT|O_APPEND);
   fdtable[STDERR_FILENO].handle = fdtable[i].handle;
   fdtable[STDERR_FILENO].pos = 0;
+}
+
+/* Initialize command line arguments. We must do it here because symbols in
+   this file are referenced in libc and the linker always pushes an
+   object file for this source file to the final binary. Anyway, it would be
+   wise to do all semihosting initializations in one place.  */
+
+#ifndef RISCV_MAX_CMDLINE_LENGTH
+#define RISCV_MAX_CMDLINE_LENGTH 1024
+#endif
+
+#ifndef RISCV_MAX_ARGV_LENGTH
+#define RISCV_MAX_ARGV_LENGTH 64
+#endif
+
+int _argc = 0;
+char *_argv[RISCV_MAX_ARGV_LENGTH] = { NULL };
+
+static void
+init_semihosting_args ()
+{
+  static char cmdline[RISCV_MAX_CMDLINE_LENGTH];
+  long args[2] = { (long) cmdline, sizeof(cmdline) };
+  long ret = syscall_errno(SEMIHOST_get_cmdline, args);
+  char *ptr;
+
+  /* Exit early if semihosting call failed or NULL buffer is returned.  */
+  if (ret != 0 || (char *) args[0] == NULL) {
+    return;
+  }
+
+  /* If a semihosting implementation returns another pointer to a command
+     line buffer and does not reuse our own one then copy that explicitly.  */
+  if (args[0] != (long) cmdline) {
+    strcpy(cmdline, (const char *) args[0]);
+  }
+
+  ptr = cmdline;
+  while (_argc < RISCV_MAX_ARGV_LENGTH - 1) {
+    /* Skip leading whitespace characters.  */
+    while (isspace(*ptr))
+      ptr++;
+
+    /* Finish if the end is reached.  */
+    if (*ptr == '\0')
+      break;
+
+    /* An argument is found.  */
+    _argv[_argc++] = ptr;
+
+    /* Skip argument's characters.  */
+    while (*ptr != '\0' && !isspace(*ptr))
+      ptr++;
+
+    /* Finish if the end is reached (again).  */
+    if (*ptr == '\0')
+      break;
+
+    /* Cut out an argument in a command line.  */
+    *ptr = '\0';
+    ptr++;
+  }
+}
+
+void __attribute__ ((constructor))
+init_semihosting ()
+{
+  init_semihosting_fdtable();
+  init_semihosting_args();
 }
 
 /* Add entry to fdtable.  */
